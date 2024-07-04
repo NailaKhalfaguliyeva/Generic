2f25138d452aa5d69101f28eb6301c2dd9e84734
Naila <khalfaguliyevanaila@gmail.com>
2024-07-04 17:23:43 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿//1)Generic Swap Method
+
+//Write a generic method that swaps two elements.
+
+//İki elementi dəyişdirən ümumi metod yazın.
+
+List<int> number = new();
+number.Add(1);
+number.Add(8);
+number.Add(13);
+number.Add(4);
+number.Remove(4);
+number.Remove(1);
+foreach (var item in number)
+{
+    Console.WriteLine(item);
+
+}
+
+
+#region TASK2
+//Generic Max Method
+
+//Write a generic method to find the maximum of two values.
+
+//Dictionary<string,int>dictionary = new Dictionary<string,int>();
+//dictionary.Add("Naila", 9);
+//dictionary.Add("Lala", 15);
+
+//foreach (var item in dictionary)
+//{
+//    Console.WriteLine(item.Value);
+//}
+
+#endregion
+
+
+#region TASK3
+//Generic Print Method
+
+//Write a generic method that prints all elements in an array.
+
+
+//List<int> numbers = new();
+//numbers.Add(9);
+//numbers.Add(13);
+//numbers.Add(12);
+//numbers.Add(45);
+//foreach (var item in numbers)
+//{
+//    Console.WriteLine(item);
+//}
+
+#endregion
+
+
+
+//4)Collections:
+
+//1)Write a generic method that prints all elements in a List<T>.
+
+//List<T>-də bütün elementləri çap edən ümumi metod yazın.
+
+
+
